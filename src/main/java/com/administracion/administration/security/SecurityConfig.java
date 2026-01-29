f7699ce264661f7867e581e00cbd49a424c52bad
jonathanProgram21 <135joni246@gmail.com>
2026-01-29 09:36:46 -0600

    acceso a front desde vercel

--- src/main/java/com/administracion/administration/security/SecurityConfig.java
+++ src/main/java/com/administracion/administration/security/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174", "https://admin-user-tan.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
